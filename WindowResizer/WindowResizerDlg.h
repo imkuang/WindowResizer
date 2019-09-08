4db5e2403a9ab33e09ab5d289d00d710fccf40c6
xirikm <xirikm@gmail.com>
2019-09-08 12:50:00 +0800

    添加项目文件。

---
+++ WindowResizer/WindowResizerDlg.h
@@ -0,0 +1,34 @@
+﻿
+// WindowResizerDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CWindowResizerDlg 对话框
+class CWindowResizerDlg : public CDialogEx
+{
+// 构造
+public:
+	CWindowResizerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WINDOWRESIZER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
